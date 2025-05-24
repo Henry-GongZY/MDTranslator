ac671b297014ea19578f288fdc743bf3ddf8a9ec
龚卓岳 <watershell@qq.com>
2025-05-24 20:39:21 +0800

    daily update!

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 
 int main() {
     // 1. 配置参数
-    std::string api_key = "AIzaSyBpYGdQa6D8lt3tSZzVTL-iOYBH7y_95Xg";
+    std::string api_key = "YOUR_API_KEY";
     BaseTranslator* translator = new GeminiTranslator(api_key);
     translator->translate("Hello!");
     return 0;
